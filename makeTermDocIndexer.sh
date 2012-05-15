3fe1cdf627932004fdb21aceaa8958448d5289ef
Jaison George <jaisong87@gmail.com>
2012-05-15 11:56:36 -0400

    Changes in termdoc indexer

--- makeTermDocIndexer.sh
+++ makeTermDocIndexer.sh
@@ -1,2 +1,2 @@
-javac -classpath $HADOOP_HOME/hadoop-core-0.20.203.0.jar -d TermDocIndexer TermDocIndexer/TermDocIndexer.java
+javac -classpath $HADOOP_HOME/hadoop-core-0.20.203.0.jar -d TermDocIndexer TermDocIndexer/org/myorg/TermDocIndexer.java
 jar -cvf bin/TermDocIndexer.jar -C TermDocIndexer/ .
