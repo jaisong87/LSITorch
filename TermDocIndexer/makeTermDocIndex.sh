39405f20a05be5c3dbfc6accecd15194d76a17c7
Jaison George <jaisong87@gmail.com>
2012-05-17 11:59:49 -0400

    make termDoc script changed

--- TermDocIndexer/makeTermDocIndex.sh
+++ TermDocIndexer/makeTermDocIndex.sh
@@ -2,4 +2,4 @@
 echo "USing hadoop from $HADOOP_HOME \n"
 HADOOP_OUT="$HADOOP_INP/../TDMatrix"
 echo "Making TermDocIndex from $HADOOP_INP into $HADOOP_OUT \n"
-$HADOOP_HOME/bin/hadoop jar ../bin/TermDocIndexer.jar org.myorg.TermDocIndexer $HADOOP_INP $TERM_COUNT
+$HADOOP_HOME/bin/hadoop jar ../bin/TermDocIndexer.jar org.myorg.TermDocIndexer $HADOOP_INP $TERM_COUNT $MAT_TYPE
